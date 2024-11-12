faca1c8f10fe60d7a1aebf51039e3b36847c4842
Venkata sai vardhan Thimmisetty <57175225+sai2408@users.noreply.github.com>
2024-11-12 15:49:39 +0530

    Add files via upload

---
+++ Day - 2 [12112024]/sample.py
@@ -0,0 +1,31 @@
+#Assignment
+a = 10
+b = 20
+print(a)
+print(b)
+#Reassignment
+a = 100
+a = 300
+print(a)
+#Multiple assignment
+a,b,c = 10,20,30
+print(a)
+print(b)
+print(c)
+a=b=c=34
+print(a)
+print(b)
+print(c)
+#Swapping of varaible
+a = 10
+b = 20
+print(a)
+print(b)
+a,b = b,a
+print(a)
+print(b)
+#Deleting Variable
+hello = 123
+print(hello)
+del hello
+print(hello)
